d0822b15a5a15a54003ac1f049e90bcc5f952900
TastyMeat <65907415+TastyMeat@users.noreply.github.com>
2023-04-26 16:02:01 -0700

    fixed for presentation

--- OpenGlTest/Player.cpp
+++ OpenGlTest/Player.cpp
@@ -1,6 +1,7 @@
 #include "Player.h"
 #include "Projectile.h"
 #include "AssetManager.h"
+#include "Obstacle.h"
 #include <cmath>
 
 const std::array<const AnimatorController*, 2> Player::animatorControllers{ &AssetManager::MiniTikeMyson_Controller, &AssetManager::BigTikeMyson_Controller }; //change to small and big guy controller
@@ -14,7 +15,7 @@ void Player::Update() {
 	float screenWidth = 340.0f;
 	float screenHeight = 340.0f;
 
-	float gravityDelayTime = .3f;
+	float gravityDelayTime = .15f;
 	const float gForce = 10.0f;
 	Vector2 gravity = Vector2::down() * gForce * Time::deltaTime();
 	if (Time::time() >= stopGravity)
@@ -31,19 +32,21 @@ void Player::Update() {
 	if (Input::GetKeyDown(KeyCode::UpArrow) || Input::GetKeyDown(KeyCode::W)) { animator->Play("Jump"); stopGravity = Time::time() + gravityDelayTime; }
 	
 	if (std::abs(transform()->position.x()) > (float)ApplicationManager::width / screenWidth) transform()->position -= horizontalMovement;
-	if (transform()->position.y() <-1) transform()->position.Set(transform()->position.x(), -1, transform()->position.z());
+	if (transform()->position.y() < -1) transform()->position.Set(transform()->position.x(), -1, transform()->position.z());
 	
 	if (Input::GetKeyDown(KeyCode::Space)) {
 		powerState = (PowerState)(((int)powerState + 1) % 2);
 		animator->SetAnimatorController(animatorControllers[(int)powerState]);
-
-		if (!renderer->enabled)renderer->enabled = true;
 	}
-	//animator->SetInteger("x", x);
 
 	animator->SetBool("isWalking", std::abs(x) > 0);
-	if (std::abs(x) > 0)transform()->scale.Set(x > 0 ? 1 : -1, transform()->scale.y(), transform()->scale.z());
+	if (std::abs(x) > 0) transform()->scale.Set(x > 0 ? 1 : -1, transform()->scale.y(), transform()->scale.z());
 
+
+	if(Input::GetKeyDown(KeyCode::R))
+		if (!renderer->enabled)renderer->enabled = true;
+
+	//animator->SetInteger("x", x);
 
 	/*
 	//308.0f
@@ -100,5 +103,9 @@ void Player::LateUpdate() {
 
 void Player::OnCollisionEnter2D(){
 	renderer->enabled = false;
+	for (auto& obstacle : FindObjectsByType<Obstacle>()) Destroy(*obstacle->gameObject);
+	powerState = PowerState::Small;
+	animator->SetAnimatorController(animatorControllers[(int)powerState]);
+
 }
 
