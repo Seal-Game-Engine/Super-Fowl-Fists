42ade12b32155d2a98837de9429881531ed10421
de-only-wei <eo99x6@gmail.com>
2023-05-11 04:23:01 -0700

    adjustment

--- OpenGlTest/GlobalGameScene.cpp
+++ OpenGlTest/GlobalGameScene.cpp
@@ -204,7 +204,7 @@ const GameObject Lv0_Platform_Object = GameObject(
 	Scene GlobalGameScene::Lv1Scene = Scene({
 	{&Lv1Scene_Object, Transform()},
 	{&GameEventManagerObject, Transform()},
-	{&Ground_Object, Transform(Vector2(0, -2))},
+	{&Ground_Object, Transform(Vector2(0, -2.3))},
 	{&MainCamera, Transform()},
 
 	{ &TikeMyson_UI_Object, Transform(Vector2(-2.5, -1.9)) },
